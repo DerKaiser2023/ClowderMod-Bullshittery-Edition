204796edc599d8d1d522f3f1e5e1b0de51778d3b
DerKaiser2023 <discordballs872@gmail.com>
2025-03-25 13:04:59 -0400

    Update RefStrings.java

--- src/main/java/com/hfr/lib/RefStrings.java
+++ src/main/java/com/hfr/lib/RefStrings.java
@@ -2,8 +2,8 @@ package com.hfr.lib;
 
 public class RefStrings {
 	public static final String MODID = "hfr";
-	public static final String NAME = "xRadar";
-	public static final String VERSION = "1.7.11";
+	public static final String NAME = "ClowderMod Bullshittery Edition";
+	public static final String VERSION = "1.0";
 	public static final String CLIENTSIDE = "com.hfr.main.ClientProxy";
 	public static final String SERVERSIDE = "com.hfr.main.ServerProxy";
 	public static final String GUI_FACTORY = "com.hfr.config.XRConfigFactory";
